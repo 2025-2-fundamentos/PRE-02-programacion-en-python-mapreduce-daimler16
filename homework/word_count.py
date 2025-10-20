c91adf3b45099c52f08b29de541f08c6da30975e
Ddddd <92930175+daimler16@users.noreply.github.com>
2025-10-20 17:15:12 -0500

    act

--- homework/word_count.py
+++ homework/word_count.py
@@ -11,14 +11,31 @@ from itertools import groupby
 from toolz.itertoolz import concat, pluck
 
 
-
 def copy_raw_files_to_input_folder(n):
     """Generate n copies of the raw files in the input folder"""
 
+    create_directory("files/input")
+
+    for file in glob.glob("files/raw/*"):
+
+        with open(file, "r", encoding="utf-8") as f:
+            text = f.read()
+
+        for i in range(1, n + 1):
+            filename = f"{os.path.basename(file).split('.')[0]}_{i}.txt"
+            with open(f"files/input/{filename}", "w", encoding="utf-8") as f2:
+                f2.write(text)
 
 
 def load_input(input_directory):
     """Funcion load_input"""
+
+    sequence = []
+    files = glob.glob(f"{input_directory}/*")
+    with fileinput.input(files=files) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    return sequence
 
 
 def preprocess_line(x):
@@ -28,6 +45,7 @@ def preprocess_line(x):
 def map_line(x):
     pass
 
+
 def mapper(sequence):
     """Mapper"""
 
@@ -36,9 +54,9 @@ def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
 
 
-
 def compute_sum_by_group(group):
     pass
+
 
 def reducer(sequence):
     """Reducer"""
@@ -46,6 +64,13 @@ def reducer(sequence):
 
 def create_directory(directory):
     """Create Output Directory"""
+
+    if os.path.exists(directory):
+        for file in glob.glob(f"{directory}/*"):
+            os.remove(file)
+        os.rmdir(directory)
+
+    os.makedirs(directory)
 
 
 def save_output(output_directory, sequence):
